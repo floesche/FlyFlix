544d858e57075deb1da8d5942da94d3bdae01ccf
Frank Loesche <loeschef@janelia.hhmi.org>
2021-07-01 17:48:13 -0400

    linting

--- Experiment/SpatialTemporal.py
+++ Experiment/SpatialTemporal.py
@@ -23,11 +23,12 @@ class SpatialTemporal():
         :param float rotate_deg_hz: Rotation speed in degree per second.
         :rtype: None
         """
-        if (bar_deg < 0 or space_deg <0):
+        if bar_deg < 0 or space_deg <0:
             warnings.warn("bar size or space is negative")
-        if (360 % (bar_deg + space_deg) != 0):
-            warnings.warn(f"Spatial pattern is not seamless with bar {bar_deg}° and space {space_deg}")
-        if (rotate_deg_hz is None):
+        if 360 % (bar_deg + space_deg) != 0:
+            warnings.warn(
+                f"Spatial pattern is not seamless with bar {bar_deg}° and space {space_deg}")
+        if rotate_deg_hz is None:
             warnings.warn("temporal components needs to be set.")
         self.bar_deg = bar_deg
         self.space_deg = space_deg
@@ -70,33 +71,79 @@ class SpatialTemporal():
 
     def get_bar_sweep_duration(self, sweep_angle_deg=180) -> float:
         """
+        Calculates the duration for a bar sweep and returns the duration in seconds.
+
+        :param float sweep_angle_deg: size of the viewing angle for the sweep. Defaults to half a
+        cylinder (180°).
+
+        :rtype: float
         """
+        # TODO: maybe convert to Duration instead of float in seconds?
         return ((sweep_angle_deg + 2*self.bar_deg) / abs(self.rotate_deg_hz))*1000
 
     def get_space_sweep_duration(self, sweep_angle_deg=180) -> float:
-        return ((sweep_angle_deg + 2* self.space_deg) / abs(self.rotate_deg_hz)*1000)
+        """
+        Calculates the duration for a space sweep and returns the duration in seconds.
+
+        :param float sweep_angle_deg: size of the viewing angle for the sweep. Defaults to half a
+        cylinder (180°).
+
+        :rtype: float
+        """
+        return (sweep_angle_deg + 2* self.space_deg) / abs(self.rotate_deg_hz)*1000
 
     def trigger_rotation(self, socket_io) -> None:
+        """
+        Triggers the start of the spatial-temporal pattern by sending the according command through
+        the socket.
+
+        :param socket socket_io: Socket used for sending the update
+        :rtype: None
+        """
         shared_key = time.time_ns()
         socket_io.emit('speed', (shared_key, math.radians(self.rotate_deg_hz)))
 
     def trigger_stop(self, socket_io) -> None:
+        """
+        Stops the movement of the spatial-temporal pattern by sending a stop command through the
+        socket.
+
+        :param socket socket_io: Socket for sending the update.
+        :rtype: None
+        """
         shared_key = time.time_ns()
         socket_io.emit('speed', (shared_key, 0))
 
     def trigger_spatial(self, socket_io) -> None:
+        """
+        Sends the bar size and space size via the socket to setup up the client.
+
+        :param socket socket_io: Socket for sending the update.
+        :rtype: None
+        """
         shared_key = time.time_ns()
-        socket_io.emit('spatial-setup', (shared_key, math.radians(self.bar_deg), math.radians(self.space_deg)))
+        socket_io.emit('spatial-setup', (
+            shared_key,
+            math.radians(self.bar_deg),
+            math.radians(self.space_deg)))
 
     def trigger_sweep_start_position(self, socket_io) -> None:
+        """
+        Rotates the pattern to the starting position for a single sweep. The starting position is
+        estimated based on the assumed viewing port size of roughly 180°, the size of bars (bright)
+        and space (dark), and the direction of movement.
+
+        :param socket socket_io: Socket for sending the update to the client.
+        :rtype: None
+        """
         shared_key = time.time_ns()
         start_angle = 0
-        if self.is_bar_sweep(): 
+        if self.is_bar_sweep():
             if self.rotate_deg_hz > 0:
                 start_angle = 90
             else:
                 start_angle = 270+self.bar_deg
-        elif self.is_space_sweep(): 
+        elif self.is_space_sweep():
             if self.rotate_deg_hz > 0:
                 start_angle = 90 - self.space_deg
             else:
@@ -106,6 +153,13 @@ class SpatialTemporal():
         socket_io.emit('rotate-to', (shared_key, math.radians(start_angle)))
 
     def trigger_closedloop_start_position(self, socket_io) -> None:
+        """
+        Rotates the pattern to a sensible starting position for a closed loop experiment, which
+        means that there is an edge between dark and bright in front of the animal.
+
+        :param socket socket_io: Socket for sending the update to the client.
+        :rtype: None
+        """
         shared_key = time.time_ns()
         start_angle = 0
         if self.is_bar_sweep():
