6bbd6bb738011a5b69acdd9dec1effbf5f83b6c9
Frank Loesche <loeschef@janelia.hhmi.org>
2021-07-07 19:15:51 -0400

    linting and documentation

--- Experiment/Duration.py
+++ Experiment/Duration.py
@@ -14,6 +14,7 @@ class Duration():
         Constructor for Duration
 
         :param float time_duration: duration in ms, default 3000 (= 3 sec)
+        :rtype: None
         """
         self.time_duration = time_duration
 
--- Experiment/OpenLoopCondition.py
+++ Experiment/OpenLoopCondition.py
@@ -1,11 +1,29 @@
+"""Open loop condition, implementing """
 import warnings
 import time
 
 from . import Duration, SpatialTemporal
 
 class OpenLoopCondition():
+    """
+    Description of open loop condition
+    """
 
-    def __init__(self, spatial_temporal=None, trial_duration=None, fps=60, pretrial_duration=Duration(500), posttrial_duration=Duration(500)) -> None:
+    def __init__(
+        self,
+        spatial_temporal=None, trial_duration=None, fps=60,
+        pretrial_duration=Duration(500), posttrial_duration=Duration(500)
+        ) -> None:
+        """
+        Constructor for open loop condition.
+
+        :param SpatialTemporal spatial_temporal: Spatial and temporal definition of the stimulus
+        :param Duration trial_duration: duration of the stimulus
+        :param float fps: frame rate of the system
+        :param Duration pretrial_duration: waiting period of the pretrial, the time when the stimulus is shown without movement
+        :param  Duration posttrial_duration: waiting period of the post trial.
+        :rtype: None
+        """
         if spatial_temporal is None:
             warnings.warn("Spatial Temporal not set")
         if trial_duration is None:
@@ -18,11 +36,24 @@ class OpenLoopCondition():
         self.posttrial_duration = posttrial_duration
         self.fps = fps
 
-    def trigger_fps(self, socket_io):
+    def trigger_fps(self, socket_io) -> None:
+        """
+        Trigger the fps setting.
+
+        :param Socket socket_io: Socket.IO used for communication with the client. It is part of
+                                 the standard interface, but not used in this particular method.
+        :rtype: None
+        """
         shared_key = time.time_ns()
         socket_io.emit('fps', (shared_key, self.fps))
 
-    def trigger(self, socket_io):
+    def trigger(self, socket_io) -> None:
+        """
+        Trigger the open loop condition. This means, that configuration is sent to the client, then the 
+
+        :param Socket socket_io: Socket.IO used for communication with the client.
+        :rtype: None
+        """
         shared_key = time.time_ns()
         socket_io.emit("meta", (shared_key, "openloop-start", 1))
         self.trigger_fps(socket_io)
