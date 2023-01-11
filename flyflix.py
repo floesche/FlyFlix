5beb180ccdcee5643addb08c5669a6e19a65fe00
Wadhwa <wadhwao@hhmi.org>
2023-01-10 19:14:09 -0500

    changes in protocol

--- flyflix.py
+++ flyflix.py
@@ -241,26 +241,26 @@ def optomotor():
                         
     ## Static
     
-    for alpha in [30]:
-        for speed in [0]:
-            for direction in [-1, 1]:
-                for clrs in [(0, 50)]:
-                    bright = clrs[1]
-                    contrast = round((clrs[1]-clrs[0])/(clrs[1]+clrs[0]), 1)
-                    fg_color = clrs[1] << 8
-                    bg_color = clrs[0] << 8
-                    rotation_speed = alpha*2*speed*direction
-                    t = Trial(
-                        counter, 
-                        bar_deg=alpha, 
-                        space_deg = (60-alpha),
-                        rotate_deg_hz=rotation_speed,
-                        openloop_duration = Duration(20000),
-                        pretrial_duration=Duration(250), posttrial_duration=Duration(250),
-                        fg_color=fg_color, bg_color=bg_color,
-                        comment=f"Rotation alpha {alpha} speed {speed} direction {direction} brightness {bright} contrast {contrast}")
-                    block.append(t)
-                    counter += 1
+    #for alpha in [30]:
+    #    for speed in [0]:
+    #        for direction in [-1, 1]:
+    #            for clrs in [(0, 50)]:
+    #                bright = clrs[1]
+    #                contrast = round((clrs[1]-clrs[0])/(clrs[1]+clrs[0]), 1)
+    #                fg_color = clrs[1] << 8
+    #                bg_color = clrs[0] << 8
+    #                rotation_speed = alpha*2*speed*direction
+    #                t = Trial(
+    #                    counter, 
+    #                    bar_deg=alpha, 
+    #                    space_deg = (60-alpha),
+    #                    rotate_deg_hz=rotation_speed,
+    #                    openloop_duration = Duration(20000),
+    #                    pretrial_duration=Duration(250), posttrial_duration=Duration(250),
+    #                    fg_color=fg_color, bg_color=bg_color,
+    #                    comment=f"Rotation alpha {alpha} speed {speed} direction {direction} brightness {bright} contrast {contrast}")
+    #                block.append(t)
+    #                counter += 1
                         
     # Oscillation
     #for alpha in [15]:
