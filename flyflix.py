f5344037f5d4c5306325e6990634bd99a5f0f016
om-git216 <56737677+om-git216@users.noreply.github.com>
2023-01-19 16:07:17 -0500

    updated protocol

--- flyflix.py
+++ flyflix.py
@@ -217,13 +217,13 @@ def optomotor():
         for speed in [5]: #specifying the tepmoral frequency (in Hz)
             for direction in [-1, 1]: #specifying the direction of movement
                 for clrs in [(0, 50), (0, 255)]: #specifying the contrast/brightness
-                    for fg_color in [0x00ff00]:#, 0xffffff]: #specifying the colors of the foreground
+                    for fg_color in [0x00ff00, 0xffffff]: #specifying the colors of the foreground
                         bright = clrs[1]
                         contrast = round((clrs[1]-clrs[0])/(clrs[1]+clrs[0]), 1)
                         if fg_color == 0x00ff00:
                             fg_color = clrs[1] << 8
-                        #elif fg_color == 0xffffff:
-                        #    fg_color = clrs[1] + (clrs[1] << 8) + (clrs[1] << 16)
+                        elif fg_color == 0xffffff:
+                            fg_color = clrs[1] + (clrs[1] << 8) + (clrs[1] << 16)
                         bg_color = clrs[0] << 8
                         rotation_speed = alpha*2*speed*direction
                         t = Trial(
@@ -241,26 +241,26 @@ def optomotor():
                         
     ## Static
     
-    #for alpha in [30]:
-    #    for speed in [0]:
-    #        for direction in [-1, 1]:
-    #            for clrs in [(0, 50)]:
-    #                bright = clrs[1]
-    #                contrast = round((clrs[1]-clrs[0])/(clrs[1]+clrs[0]), 1)
-    #                fg_color = clrs[1] << 8
-    #                bg_color = clrs[0] << 8
-    #                rotation_speed = alpha*2*speed*direction
-    #                t = Trial(
-    #                    counter, 
-    #                    bar_deg=alpha, 
-    #                    space_deg = (60-alpha),
-    #                    rotate_deg_hz=rotation_speed,
-    #                    openloop_duration = Duration(20000),
-    #                    pretrial_duration=Duration(250), posttrial_duration=Duration(250),
-    #                    fg_color=fg_color, bg_color=bg_color,
-    #                    comment=f"Rotation alpha {alpha} speed {speed} direction {direction} brightness {bright} contrast {contrast}")
-    #                block.append(t)
-    #                counter += 1
+    for alpha in [30]:
+        for speed in [0]:
+            for direction in [-1, 1]:
+                for clrs in [(0, 50)]:
+                    bright = clrs[1]
+                    contrast = round((clrs[1]-clrs[0])/(clrs[1]+clrs[0]), 1)
+                    fg_color = clrs[1] << 8
+                    bg_color = clrs[0] << 8
+                    rotation_speed = alpha*2*speed*direction
+                    t = Trial(
+                        counter, 
+                        bar_deg=alpha, 
+                        space_deg = (60-alpha),
+                        rotate_deg_hz=rotation_speed,
+                        openloop_duration = Duration(20000),
+                        pretrial_duration=Duration(250), posttrial_duration=Duration(250),
+                        fg_color=fg_color, bg_color=bg_color,
+                        comment=f"Rotation alpha {alpha} speed {speed} direction {direction} brightness {bright} contrast {contrast}")
+                    block.append(t)
+                    counter += 1
                         
     # Oscillation
     #for alpha in [15]:
