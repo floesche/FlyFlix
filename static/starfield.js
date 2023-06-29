0129e4d300e8746670bf28f1a164041c1db64b38
quinnwall <136604168+quinnwall@users.noreply.github.com>
2023-06-29 15:39:50 -0400

    adding multiple spheres with motion
    created an array of spheres with uniform x positions. an animate function moves them slowly to the right

--- static/starfield.js
+++ static/starfield.js
@@ -20,7 +20,8 @@ import { Group,
 let scene;
 let camera;
 let renderer;
-let sphere;
+const sphereCount = 5;
+let spheres = [];
 
 function main(){
 
@@ -30,22 +31,30 @@ function main(){
     renderer = createRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
 
-
+    // define the container and add the renderer
     const container = document.querySelector('#scene-container');
     container.append( renderer.domElement );
 
+    // create a sphere
     const geometry = new SphereGeometry( 1, 32, 16 );
     const material = new MeshBasicMaterial( { color: 0x00ff00 } );
-    sphere = new Mesh( geometry, material );
-    sphere.position.x = 3
-    scene.add( sphere );
 
-    camera.position.z = 5;
+    for ( let i=0; i<sphereCount; i++){
+        let sphereMesh = new Mesh( geometry, material );
+        sphereMesh.position.x = -6 + 3*i;
+        spheres.push(sphereMesh);
+        scene.add(sphereMesh);
+    }
+
+    camera.position.z = 10;
 
     function animate() {
 	    requestAnimationFrame( animate );
-        sphere.rotation.x += 0.01;
-        sphere.rotation.y += 0.01;
+        for (let k=0; k<spheres.length; k++){
+            spheres[k].position.x += 0.01;
+        }
+        //sphere.rotation.x += 0.01;
+        //sphere.rotation.y += 0.01;
 	    renderer.render( scene, camera );
     }
 
