f8b43c32f2f774f1d0a52a2b13dcd685a9543a27
quinnwall <136604168+quinnwall@users.noreply.github.com>
2023-07-05 15:26:07 -0400

    Datetime reformat when reading config file for metadata - Fixes #17 (#18)
    * New ISO formatting for datetime items in config file. Fixes #17
    
    Fixes an issue that caused the control panel to have an internal server error when datetime datatypes were passed in as metadata. Implements a method called metadata_as_string that uses the datetime function isoformat() to convert the dates to strings.
    
    * Adding scroll bar to control panel and fixing resizing error from #13
    
    This changes the styling in control-panel.html to not have overlapping elements depending on window sizes. It also implements a scroll bar if the content does not fit on the screen. May not be the best permanent fix but it addresses the main bug.
    
    * Date or datetime now works with control panel - #17
    
    Both date and datetime datatypes are now converted to strings through data_as_string. However, there are still some weaknesses including incompatibility with additional datetime datatypes. This commit also loads the data from the config file into filedata before passing it into metadata. No progress has been made on making the data_as_string function more efficient.
    
    * Compatability with all time types
    
    Ensures that all datetime data types work with the data_as_string function

--- flyflix.py
+++ flyflix.py
@@ -13,6 +13,8 @@ from pathlib import Path
 from logging import FileHandler
 
 import yaml
+import datetime
+
 
 import eventlet
 
@@ -62,11 +64,44 @@ def read_metadata():
     with open("defaultsconfig.yaml", "r") as stream:
         try:
             with metadata_lock:
-                metadata = yaml.safe_load(stream)
+                filedata = yaml.safe_load(stream)
+                metadata = data_as_string(filedata)
             print(metadata)
         except yaml.YAMLError as exc:
             print(exc)
 
+
+def data_as_string(dict):
+    """
+    reformats the data so that dates are saved as strings in ISO format
+    
+    { delKey } - list of keys in metadata that need to be deleted
+    { f_pairs } - dictionary with reformatted keys that needs to replace old keys
+    
+    """
+    delKey = []
+    f_pairs = {}
+    for key in dict:
+        val = dict[key]
+        key_type = type(key)
+        val_type = type(dict[key])
+        if (val_type == datetime.date or val_type == datetime.datetime or val_type == datetime.time):
+            val = val.isoformat()
+            dict[key] = val
+        if (key_type == datetime.date or key_type == datetime.datetime or key_type == datetime.time):
+            key_f = key.isoformat()
+            f_pairs[key_f] = val
+            delKey.append(key)
+    
+    #deletes all keys in datetime format
+    for key in delKey:
+        del dict[key]
+        
+    #adds keys that were reformatted to ISO
+    dict.update(f_pairs)
+    
+    return dict
+        
 
 def before_first_request():
     """
