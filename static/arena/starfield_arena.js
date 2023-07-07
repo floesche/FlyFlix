4786d95e39a78c7129f74c1e4c4ba67b275896b7
quinnwall <136604168+quinnwall@users.noreply.github.com>
2023-07-07 13:55:36 -0400

    Broken sphere class
    This a sphere class that is not functional, do not use this version unless you are attempting to fix it.

--- static/arena/starfield_arena.js
+++ static/arena/starfield_arena.js
@@ -16,7 +16,7 @@ import { Group,
     WebGLRenderer,
     BoxGeometry,
 } from '/static/vendor/three.module.js';
-import { createSpheres } from './components/spheres.js';
+import { Spheres } from './components/spheres.js';
 
 
 let scene;
@@ -40,7 +40,7 @@ class StarfieldArena {
         container.append( renderer.domElement );
 
         // create a group of spheres
-        spheres = createSpheres(sphereCount, sphereRadius);
+        spheres = new Spheres(sphereCount, sphereRadius, 10);
         scene.add(spheres);
 
         //camera.position.z = 20;
@@ -57,7 +57,7 @@ class StarfieldArena {
     }
 
     tick(){
-        spheres.rotateX(-0.01);
+        spheres.tick(clock.getDelta());
     }
 
 
