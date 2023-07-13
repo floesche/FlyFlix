95ed5449bf9bad7184d8e02e294ceaa3605d3ceb
quinnwall <136604168+quinnwall@users.noreply.github.com>
2023-07-13 13:29:30 -0400

    Implementing loop class for starfield animation
    Updating starfield_arena to use the loop class to start/stop animations instead of starfield_arena handling animation.

--- static/arena/starfield_arena.js
+++ static/arena/starfield_arena.js
@@ -4,6 +4,7 @@ import { createStarfieldCamera } from './components/starfield_camera.js';
 import { createRenderer } from './systems/renderer.js';
 import { Resizer } from './systems/resizer.js';
 import { Spheres } from './components/spheres.js';
+import { Loop } from './systems/loop.js';
 
 
 let scene;
@@ -24,26 +25,41 @@ class StarfieldArena {
         camera = createStarfieldCamera();
         renderer = createRenderer();
 
+        //loop to animate the scene
+        loop = new Loop(camera, scene, renderer);
+
         // add the renderer
         container.append( renderer.domElement );
 
         // create an object made up of a group of spheres;
         spheres = new Spheres(sphereCount, sphereRadius, shellRadius);
         scene.add(spheres);
+        loop.updateables.push(spheres);
         
         const resizer = new Resizer(container, camera, renderer);
     }
     
     
-    start() {
-        renderer.setAnimationLoop(() => {
-            this.tick();
-            renderer.render( scene, camera );
-        });
+    /**
+     * Render the arena.
+     * 
+     */
+    render() {
+        renderer.render(scene, camera);
     }
 
-    tick(){
-        spheres.tick();
+    /**
+     * Start the animation of the arena.
+     */
+    start() {
+        loop.start();
+    }
+
+    /**
+     * Stop the animation of the arena.
+     */
+    stop() {
+        loop.stop();
     }
 
 }
