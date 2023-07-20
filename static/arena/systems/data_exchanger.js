c6dbc1ad6fcd9d99408d14138b8ee11d1d61f2ad
quinnwall <136604168+quinnwall@users.noreply.github.com>
2023-07-20 09:38:00 -0400

    Updating event names
    Updated event names so that spheres and panels do not require separate data exchanger classes.

--- static/arena/systems/data_exchanger.js
+++ static/arena/systems/data_exchanger.js
@@ -11,8 +11,10 @@ class DataExchanger{
      * @param {Scene} scene
      * @param {Loop} loop - The animation loop
      * @param {Panels} panels - the group of panels
+     * @param {Spheres} spheres - the group of spheres
+     * @param {Mask} masks - the mask of the arena
      */
-    constructor(camera, scene, loop, panels, masks){
+    constructor(camera, scene, loop, panels, spheres, masks){
 
         // The data exchanger connects to a Socket IO at port 17000
         const socketurl = window.location.hostname + ":17000";
@@ -43,16 +45,28 @@ class DataExchanger{
          * @param {bigint} lid - Loop ID
          * @param {number} speed - set rotational speed for panels in radians per second
          */
-        this.socket.on('speed', (lid, speed) => {
+        this.socket.on('panels-speed', (lid, speed) => {
             panels.setLid(lid);
             panels.setRotateRadHz(speed);
             this.log(lid, 'de-panel-speed', speed);
         });
 
-        this.socket.on('oscillation', (lid, osc_freq, osc_width) => {
+        this.socket.on('spheres-speed', (lid, speed) => {
+            spheres.setLid(lid);
+            spheres.setRotateRadHz(speed);
+            this.log(lid, 'de-sphere-speed', speed);
+        });
+
+        this.socket.on('panels-oscillation', (lid, osc_freq, osc_width) => {
             panels.setLid(lid);
             panels.setOscillation(osc_freq, osc_width);
             this.log(lid, 'de-panels-oscillation', osc_freq);
+        });
+
+        this.socket.on('spheres-oscillation', (lid, osc_freq, osc_width) => {
+            spheres.setLid(lid);
+            spheres.setOscillation(osc_freq, osc_width);
+            this.log(lid, 'de-spheres-oscillation', osc_freq);
         });
 
         /**
@@ -71,10 +85,16 @@ class DataExchanger{
          * @param {bigint} lid - Loop ID
          * @param {number} targetRotationRad - target rotation in radians
          */
-        this.socket.on('rotate-to', (lid, targetRotationRad) => {
+        this.socket.on('panels-rotate-to', (lid, targetRotationRad) => {
             panels.setLid(lid);
             panels.setRotationRad(targetRotationRad);
             this.log(lid, 'de-rotate-panel-to', targetRotationRad);
+        })
+
+        this.socket.on('spheres-rotate-to', (lid, targetRotationRad) => {
+            spheres.setLid(lid);
+            spheres.setRotationRad(targetRotationRad);
+            this.log(lid, 'de-rotate-spheres-to', targetRotationRad);
         })
 
         /**
@@ -90,26 +110,49 @@ class DataExchanger{
         });
 
         /**
-         * Event handler for `spatial-setup` message. The cylindrical arena consists of vertical bars interleaved with spaces. All bars have the same width as all intervals do. The bars and spaces are repeated until the fill cylinder is filled. To have an arena with four bars spanning one eighth of the cylinder, barWidth and spaceWidth could both be defined as pi/4. To have a single bright bar that spans a quarter of an arena, barWidth could be pi/2 and spaceWidth pi*3/2.
+         * Event handler for `panels-spatial-setup` message. The cylindrical arena consists of vertical bars interleaved with spaces. All bars have the same width as all intervals do. The bars and spaces are repeated until the fill cylinder is filled. To have an arena with four bars spanning one eighth of the cylinder, barWidth and spaceWidth could both be defined as pi/4. To have a single bright bar that spans a quarter of an arena, barWidth could be pi/2 and spaceWidth pi*3/2.
          * 
          * @param {bigint} lid -Loop ID
          * @param {number} barWidth - bar width in radians
          * @param {number} spaceWidth - interval width between bars in radians
          */
-        this.socket.on('spatial-setup', (lid, barWidth, spaceWidth, maskStart, maskEnd, fgColor, bgColor, barHeight) => {
+        this.socket.on('panels-spatial-setup', (lid, barWidth, spaceWidth, maskStart, maskEnd, fgColor, bgColor, barHeight) => {
             panels.setLid(lid);
             panels.changePanels(barWidth, spaceWidth, fgColor, bgColor, barHeight);
             //scene.changeBgColor(bgColor);
             scene.background = new Color(bgColor);
             masks.setLid(lid);
             masks.changeMask(maskStart+mr, maskEnd+mr, bgColor);
-            this.log(lid, 'de-spatial-setup-bar', barWidth);
-            this.log(lid, 'de-spatial-setup-space', spaceWidth);
-            this.log(lid, 'de-spatial-setup-mask-start', maskStart);
-            this.log(lid, 'de-spatial-setup-mask-end', maskEnd);
-            this.log(lid, 'de-spatial-setup-fgcolor', fgColor);
-            this.log(lid, 'de-spatial-setup-bgcolor', bgColor);
-            this.log(lid, 'de-spatial-setup-barheight', barHeight);
+            this.log(lid, 'de-panels-spatial-setup-bar', barWidth);
+            this.log(lid, 'de-panels-spatial-setup-space', spaceWidth);
+            this.log(lid, 'de-panels-spatial-setup-mask-start', maskStart);
+            this.log(lid, 'de-panels-spatial-setup-mask-end', maskEnd);
+            this.log(lid, 'de-panels-spatial-setup-fgcolor', fgColor);
+            this.log(lid, 'de-panels-spatial-setup-bgcolor', bgColor);
+            this.log(lid, 'de-panels-spatial-setup-barheight', barHeight);
+        });
+
+        /**
+         * Event handler for `spheres-spatial-setup` message. The spherical arena consists of spheres. 
+         * Spheres have different radii depending on their position due to distortion.
+         * 
+         * @param {bigint} lid -Loop ID
+         * Interface to change the sphere setup
+         * @param {number} sphereCount - the amount of total spheres surrounding the fly
+         * @param {number} sphereRadius - the radius of the spheres in the starfield
+         * @param {number} shellRadius - the radius of the shell / distance between camera and spheres
+         * @param {color} color - color of the spheres (default is green)
+        */
+        this.socket.on('spheres-spatial-setup', (lid, sphereCount, sphereRadius, shellRadius, seed, positionList, color) => {
+            var positions = JSON.parse(positionList)
+            spheres.setLid(lid);
+            spheres.changeSpheres(sphereCount, sphereRadius, shellRadius, seed, positions, color);
+            this.log(lid, 'de-spheres-spatial-setup-sphereCount', sphereCount);
+            this.log(lid, 'de-spheres-spatial-setup-color', color);
+            this.log(lid, 'de-spheres-spatial-setup-sphereRadius', sphereRadius);
+            this.log(lid, 'de-spheres-spatial-setup-shellRadius', shellRadius);
+            this.log(lid, 'de-spheres-spatial-setup-seed', seed)
+            loop.start();
         });
 
         /**
