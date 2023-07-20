6e9ed1315cdea0e86293b449db35e1504121b77c
quinnwall <136604168+quinnwall@users.noreply.github.com>
2023-07-20 10:53:20 -0400

    Change arena
    Change the arena used in starfield experiment.

--- static/stars.js
+++ static/stars.js
@@ -1,5 +1,5 @@
 
-import { StarfieldArena } from './arena/starfield_arena.js';
+import { Arena } from './arena/arena.js';
 
 import { FullScreener } from './arena/systems/full_screener.js';
 import { ExperimentControl } from './arena/systems/experiment_control.js';
@@ -12,7 +12,7 @@ var socket = io();
 function main() {
     const container = document.querySelector('#scene-container');
   
-    const arena = new StarfieldArena(container);
+    const arena = new Arena(container);
 
     const fullScreenButton = new FullScreener(container);
     const experimentController = new ExperimentControl(container, socket);
